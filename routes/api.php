6501280ba1e02eaed7dcaf871851dbe9b0b601b7
caoting1 <caoting1@globalegrow.com>
2018-04-28 18:35:07 +0800

    话题回复

--- routes/api.php
+++ routes/api.php
@@ -76,29 +76,32 @@ $api->version('v1', [
     //需要验证token的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
 
-        // 当前登录用户信息
-        $api->get('user', 'UsersController@me')
-            ->name('api.user.show');
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
 
-        //编辑登录用户的信息
-        $api->patch('user', 'UsersController@update')
-            ->name('api.user.update');
+            //编辑登录用户的信息
+            $api->patch('user', 'UsersController@update')
+                ->name('api.user.update');
 
-        //图片资源
-        $api->post('images', 'ImagesController@store')
-            ->name('api.images.store');
+            //图片资源
+            $api->post('images', 'ImagesController@store')
+                ->name('api.images.store');
 
-        //发布话题
-        $api->post('topics', 'TopicsController@store')
-            ->name('api.topics.store');
+            //发布话题
+            $api->post('topics', 'TopicsController@store')
+                ->name('api.topics.store');
 
-            //修改话题
-        $api->patch('topics/{topic}', 'TopicsController@update')
-            ->name('api.topics.update');
-        });
+                //修改话题
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
+            });
 
-        $api->delete('topics/{topic}', 'TopicsController@destroy')
-            ->name('api.topics.destroy');
+            $api->delete('topics/{topic}', 'TopicsController@destroy')
+                ->name('api.topics.destroy');
+
+            $api->post('topics/{topic}/replies', 'RepliesController@store')
+                ->name('api.topics.replies.store');
 
 });
 
