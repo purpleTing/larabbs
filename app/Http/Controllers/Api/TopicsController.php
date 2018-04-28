15c4c1c79e1fc5b98cd089a252861b9bc2043846
caoting1 <caoting1@globalegrow.com>
2018-04-28 11:52:47 +0800

    删除话题

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -21,9 +21,17 @@ class TopicsController extends Controller
 
     public function update(TopicsRequest $request, Topic $topic)
     {
-//        $this->authorize('update', $topic);
+        $this->authorize('update', $topic);
         $topic->update($request->all());
 
         return $this->response->item($topic, new TopicsTransformer());
+    }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->delete();
+
+        return $this->response->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -85,7 +85,8 @@ $api->version('v1', [
             ->name('api.topics.update');
         });
 
-
+        $api->delete('topics/{topic}', 'TopicsController@destroy')
+            ->name('api.topics.destroy');
 
 });
 
