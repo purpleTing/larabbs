30106fc555ff34dc1c29c19ca692e8be05540bda
caoting1 <caoting1@globalegrow.com>
2018-05-03 11:32:02 +0800

    消息通知

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,7 +16,11 @@ class EventServiceProvider extends ServiceProvider
          \SocialiteProviders\Manager\SocialiteWasCalled::class => [
         // add your listeners (aka providers) here
         'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
-    ],
+        ],
+
+         'eloquent.created: Illuminate\Notifications\DatabaseNotification' => [
+             'App\Listeners\PushNotification',
+         ],
     ];
 
     /**
